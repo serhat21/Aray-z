31dd57323e82f01dc4666c7f63c970dfffc421c5
miniğim <miniim@192.168.14.141>
2016-07-28 16:47:24 +0300

    Merge origin/master
    Conflicts:
    	src/java2016/Java2016.java

---
+++ src/IArayuz/IArayuz.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IArayuz;
+
+/**
+ *
+ * @author miniğim
+ */
+interface IArayuz {
+
+    void Yaz();
+    int Topla(int a,int b);
+    
+    
+}
